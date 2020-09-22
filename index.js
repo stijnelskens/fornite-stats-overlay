a9bf294d4660d241364e9a4eab88df0dadc615cd
Stijn Elskens <stijn.elskens@gmail.com>
2020-09-22 22:11:43 +0200

    updates ✨

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ function getStats(inputValue) {
     axios.get(`https://fortnite-api.com/v1/stats/br/v2/${inputValue}`)
     .then(function (response) {
         // handle success
-        console.log(response);
-
         const wins = document.getElementById('wins');
         const kills = document.getElementById('kills');
         const kd = document.getElementById('kd');
@@ -26,8 +24,35 @@ function getStats(inputValue) {
         console.log(error);
     })
     .then(function () {
-        // always executed
-        console.log('ajuuu lastest');
+        setTimeout(function() {
+            var sgMessage = document.getElementById('sg-message');
+            fadeIn(sgMessage);
+            function fadeIn(el, display) {
+                el.style.opacity = 0;
+                el.style.display = display || "block";
+                (function fade() {
+                    var val = parseFloat(el.style.opacity);
+                    if (!((val += .05) > 1)) {
+                        el.style.opacity = val;
+                        requestAnimationFrame(fade);
+                    }
+                })();
+            };
+        }, 3000);
+        setTimeout(function() {
+            var sgMessage = document.getElementById('sg-message');
+            fadeOut(sgMessage);
+            function fadeOut(e) {
+                e.style.opacity = 1;
+                (function fade() {
+                    if ((e.style.opacity -= .05) < 0) {
+                        e.style.display = "none";
+                    } else {
+                        requestAnimationFrame(fade);
+                    }
+                })();
+            };
+        }, 6500);
   });
 }
   
